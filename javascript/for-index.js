2c5ab51c00efd92f72f38ccf30d713d718d7a517
SeanJia <zjia@ucsd.edu>
2016-04-13 22:26:34 -0700

    update

--- javascript/for-index.js
+++ javascript/for-index.js
@@ -1,12 +1,12 @@
 
 /* for scroll spying of navbar */
-$(document).ready(function(){
-    $("body").scrollspy({target: "#navbar", offset: 100});
+$(document).ready(function() {
+    $("body").scrollspy({target: "#navbar", offset: 50});
 });
 
 /* for sticky navbar on the top */
 $(document).ready(function() {
-    var navbar = $("#navbar");
+    var navbar = $("#navbar-dynamic");
     function scroll() {
         var theTop = navbar.offset().top;
 
@@ -14,10 +14,12 @@ $(document).ready(function() {
             $("#navbar").addClass("navbar-fixed-top");
             $("#fixed-top").addClass("navbar-padding");
             $("#nav-right").addClass("navbar-right-margin");
+            //console.log("fix: " + theTop + " " + $(window).scrollTop());
+
         } else {
             $("#navbar").removeClass("navbar-fixed-top");
             $("#fixed-top").removeClass("navbar-padding");
-            $("#nav-right").removeClass("navbar-right-margin")
+            $("#nav-right").removeClass("navbar-right-margin");
         }
     }
     document.onscroll = scroll;
